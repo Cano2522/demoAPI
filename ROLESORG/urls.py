bcf09f4d0cf019ca4100e72dddcb603c6e6140f9
Jose <jose.cano@consulting.construction.corp>
2022-07-13 11:41:22 -0500

    se agregaron 3 endpoins  de consulta a api ROLESORG

--- ROLESORG/urls.py
+++ ROLESORG/urls.py
@@ -4,6 +4,8 @@ from ROLESORG.views import *
 router = DefaultRouter()
 
 router.register('RolesOrg', RolesOrg, basename = 'RolesOrg')
-
+router.register('ListarRolesXOMC23N2', ListarRolesXOMC23N2, basename = 'ListarRolesXOMC23N2')
+router.register('ListarRolesXOMC23N3', ListarRolesXOMC23N3, basename = 'ListarRolesXOMC23N3')
+router.register('ListarRolesXOMC23N4', ListarRolesXOMC23N4, basename = 'ListarRolesXOMC23N4')
 
 urlpatterns = router.urls
--- ROLESORG/views.py
+++ ROLESORG/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -38,4 +39,52 @@ class RolesOrg(Authentication,viewsets.ModelViewSet):
             return Response({'mensaje':'Registro eliminado correctamente'}, status = status.HTTP_200_OK)
         return Response({'error':'No existe un Registro con estos datos'}, status = status.HTTP_404_NOT_FOUND)
 
+class ListarRolesXOMC23N2(Authentication, viewsets.GenericViewSet):
+    def get_queryset(self):
+        with connection.cursor() as cursor:
+            registros = cursor.execute("SELECT Omc34Nivel2 .Codigo,Omc34Nivel2.descriEng AS descriEngOMC,Omc34Nivel2.descriSpa AS descriSpaOMC,Omc34Nivel2.anioReg,Omc34Nivel2.regFinal,RolesOrg.Codigo AS codigoOmc,RolesOrg.Consecutivo,RolesOrg.descriEng AS descriEngRol,RolesOrg.descriSpa AS descriSpaRol,RolesOrg.definicionEng,RolesOrg.definicionSpa,RolesOrg.fuenteInf,RolesOrg.fecRegInf FROM Omc34Nivel2 JOIN RolesOrg ON Omc34Nivel2.Codigo=RolesOrg.Codigo")
+            registros = dictfetchall(cursor)
+            return registros
+        
+    def list(self, request):
+        data = self.get_queryset()
+        if data:
+            return Response(data, status = status.HTTP_200_OK)
+        else:
+            return Response({'mensaje':'No existen registros!'}, status = status.HTTP_404_NOT_FOUND)
 
+class ListarRolesXOMC23N3(Authentication, viewsets.GenericViewSet):
+    def get_queryset(self):
+        with connection.cursor() as cursor:
+            registros = cursor.execute("SELECT Omc34Nivel3.Codigo,Omc34Nivel3.descriEng AS descriEngOMC,Omc34Nivel3.descriSpa AS descriSpaOMC,Omc34Nivel3.anioReg,Omc34Nivel3.regFinal,RolesOrg.Codigo AS codigoOmc,RolesOrg.Consecutivo,RolesOrg.descriEng AS descriEngRol,RolesOrg.descriSpa AS descriSpaRol,RolesOrg.definicionEng,RolesOrg.definicionSpa,RolesOrg.fuenteInf,RolesOrg.fecRegInf FROM Omc34Nivel3 JOIN RolesOrg ON Omc34Nivel3.Codigo=RolesOrg.Codigo")
+            registros = dictfetchall(cursor)
+            return registros
+        
+    def list(self, request):
+        data = self.get_queryset()
+        if data:
+            return Response(data, status = status.HTTP_200_OK)
+        else:
+            return Response({'mensaje':'No existen registros!'}, status = status.HTTP_404_NOT_FOUND)
+
+class ListarRolesXOMC23N4(Authentication, viewsets.GenericViewSet):
+    def get_queryset(self):
+        with connection.cursor() as cursor:
+            registros = cursor.execute("SELECT Omc34Nivel4.Codigo,Omc34Nivel4.descriEng AS descriEngOMC,Omc34Nivel4 .descriSpa AS descriSpaOMC,Omc34Nivel4.anioReg,Omc34Nivel4.regFinal,RolesOrg.Codigo AS codigoOmc,RolesOrg.Consecutivo,RolesOrg.descriEng AS descriEngRol,RolesOrg.descriSpa AS descriSpaRol,RolesOrg.definicionEng,RolesOrg.definicionSpa,RolesOrg.fuenteInf,RolesOrg.fecRegInf FROM Omc34Nivel4 JOIN RolesOrg ON Omc34Nivel4.Codigo=RolesOrg.Codigo")
+            registros = dictfetchall(cursor)
+            return registros
+        
+    def list(self, request):
+        data = self.get_queryset()
+        if data:
+            return Response(data, status = status.HTTP_200_OK)
+        else:
+            return Response({'mensaje':'No existen registros!'}, status = status.HTTP_404_NOT_FOUND)
+            
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
